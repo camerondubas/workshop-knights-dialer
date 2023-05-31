5ef5bd236d187dac8e398605441a90d34167590a
Cameron Dubas <camerondubas@gmail.com>
2023-05-31 15:57:26 +0300

    wip

--- js/dialer.js
+++ js/dialer.js
@@ -1,39 +1,82 @@
 export default {
-	reachableKeys,
-	countPaths,
-	listAcyclicPaths
+  reachableKeys,
+  countPaths,
+  listAcyclicPaths,
 };
-
 
 // ****************************
 
+var nearbyKeys = [
+  [4, 6],
+  [6, 8],
+  [7, 9],
+  [4, 8],
+  [3, 9, 0],
+  [],
+  [1, 7, 0],
+  [2, 6],
+  [1, 3],
+  [2, 4],
+];
+
 function reachableKeys(startingDigit) {
-	// TODO: return which digits a Knight's move
-	// can hop to from a given starting digit/key
-	//
-	// e.g. 3 -> [ 4, 8 ]
-	//      4 -> [ 3, 9, 0 ]
-	//      5 -> []
-	return [];
+  return nearbyKeys[startingDigit];
 }
 
-function countPaths(startingDigit,hopCount) {
-	// TODO: given the digit/key to start from and
-	// the number of hops to take, return a count
-	// of all the possible paths that could be
-	// traversed
-	return 0;
+function countPaths(startingDigit, hopCount) {
+  if (hopCount == 0) return 1;
+  // set up an array holding the cumulative counts for paths,
+  // starting with each of the 10 digits; initialized to 1 for
+  // each digit
+  var priorPathCounts = Array(10).fill(1);
+  for (let hops = 0; hops < hopCount; hops++) {
+    // counts for each digit for just the next hop
+    let pathCounts = Array(10).fill(0);
+    // process all 10 digits
+    for (let digit = 0; digit <= 9; digit++) {
+      // but only update the counts for each of those
+      // digits' nearby keys (i.e., valid moves)
+      for (let n of nearbyKeys[digit]) {
+        pathCounts[digit] += priorPathCounts[n];
+      }
+    }
+    // preserve the running counts for the next iteration (if any)
+    priorPathCounts = pathCounts;
+  }
+  // return only the count for the requested digit (even though
+  // all counts were computed)
+  return priorPathCounts[startingDigit];
 }
 
 function listAcyclicPaths(startingDigit) {
-	// TODO: given the digit/key to start from,
-	// return a list of the distinct acyclic
-	// paths that are possible to traverse
-	//
-	// e.g. [
-	//   [4, 3, 8, 1, 6, 7, 2, 9],
-	//   [4, 3, 8, 1, 6, 0],
-	//   ...
-	// ]
-	return [];
+  var paths = [];
+  var nextHops = nearbyKeys[startingDigit];
+  for (let nextHop of nextHops) {
+    // init a new path
+    let path = [startingDigit, nextHop];
+    // follow the path as far as possible
+    followPath(path, paths);
+  }
+  return paths;
+}
+
+function followPath(path, paths) {
+  var nextHops = nearbyKeys[path[path.length - 1]];
+  var pathForwardFound = false;
+  for (let nextHop of nextHops) {
+    // is this next key NOT already in the collected
+    // path?
+    if (!path.includes(nextHop)) {
+      pathForwardFound = true;
+      let nextPath = [...path, nextHop];
+      // recursively keep following the path
+      // as far as possible
+      followPath(nextPath, paths);
+    }
+  }
+  // no forward paths found?
+  if (!pathForwardFound) {
+    // this path is complete, so save it
+    paths.push(path);
+  }
 }
